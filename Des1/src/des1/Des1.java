7ab143859c0ed287c6d02466655444ad68fda361
DanielVallejo <ganheles@gmail.com>
2015-10-29 21:06:50 -0500

    Update Des1.java

--- Des1/src/des1/Des1.java
+++ Des1/src/des1/Des1.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package des1;
-
+import java.util.Scanner;
 import javax.swing.JOptionPane;
 
 /**
@@ -15,6 +15,16 @@ public class Des1 {
     /**
      * @param args the command line arguments
      */
+      static Scanner tecla = new Scanner(System.in);
+    public static void suma(){
+        int a,b,c;
+         System.out.print ("Ingrese a:");
+         a=Integer.valueOf(tecla.nextLine());
+         System.out.print ("Ingrese b:");
+         b=Integer.valueOf(tecla.nextLine());
+        c=a+b;
+        System.out.println(c);
+    }
     public static void main(String[] args) {
         // TODO code application logic here
         JOptionPane.showConfirmDialog(null, "HOLA");
